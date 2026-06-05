118bf829b2ac099de2b7fc0191bf908c8b8f2c5e
BigPineapple-maker <dasca.chris@gmail.com>
2026-06-05 21:37:45 +1000

    commit

--- garden-planner-react/vite.config.ts
+++ garden-planner-react/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()]
+  /* local set up
     server: {
     host: true,   // exposes on local network
     port: 5173
     }
+    */
   })
