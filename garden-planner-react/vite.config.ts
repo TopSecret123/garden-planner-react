5a70923b2b772bdd400bff60e6f293da449060d5
BigPineapple-maker <dasca.chris@gmail.com>
2026-06-05 19:55:28 +1000

    improved mobile experience. Side panels to 0 width on collapse. Moved canvastoolbar off screen as a temporary solution.

--- garden-planner-react/vite.config.ts
+++ garden-planner-react/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+    host: true,   // exposes on local network
+    port: 5173
+    }
+  })
